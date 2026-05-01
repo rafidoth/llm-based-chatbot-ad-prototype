efae711ac9e6710c9a007f8be40de0a7d8253391
rafidoth <rafiulhasan803@gmail.com>
2026-05-02 02:03:58 +0600

    db: add migrations for ad targeting mode and right ad panel

---
+++ prisma/migrations/20260501121000_add_ad_targeting_mode/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "adTargetingMode" TEXT NOT NULL DEFAULT 'turn';
---
+++ prisma/migrations/20260502103000_add_right_ad_panel/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Conversation" ADD COLUMN "right_ad_panel" BOOLEAN NOT NULL DEFAULT false;
